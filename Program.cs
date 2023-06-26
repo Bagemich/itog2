248e136032a180003b519a3089818b5e772fb0ee
znako <127133685+Bagemich@users.noreply.github.com>
2023-06-26 21:05:04 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write("Введите количество строк: ");
+int n = int.Parse(Console.ReadLine());
+  string[] originalArray = new string[n];
+        string[] newArray;
+
+        Console.WriteLine("Введите строки:");
+        for (int i = 0; i < n; i++)
+        {
+            originalArray[i] = Console.ReadLine();
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        newArray = new string[count];
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                newArray[j] = originalArray[i];
+                j++;
+            }
+        }
+
+        Console.WriteLine("Исходный массив:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(originalArray[i]);
+        }
+
+        Console.WriteLine("Новый массив:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+
+        Console.ReadLine();
+    
